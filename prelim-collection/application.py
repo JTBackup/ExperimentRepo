9bee1381f16203f7fe5daf22fcd644779cd79234
Ubuntu <ubuntu@ip-172-31-3-164.us-east-2.compute.internal>
2024-10-07 22:43:53 +0000

    workingver

--- prelim-collection/application.py
+++ prelim-collection/application.py
@@ -190,4 +190,5 @@ def handle_demographics():
 
 
 if __name__ == '__main__':
-    application.run(debug=True, host='0.0.0.0', port=5000, ssl_context=(cert.pem, key.pem))
+    application.run(debug=True, host='0.0.0.0', port=5000)
+    #application.run(debug=True, host='0.0.0.0', port=5000, ssl_context=(cert.pem, key.pem))
